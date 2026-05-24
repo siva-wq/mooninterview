2330e62e347b7180ba979b0592d6fcb75a6fc223
siva-wq <vullangulavenkatasivagangadhar@gmail.com>
2026-05-24 19:49:48 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import AppRoutes from './routes/AppRoutes';
-import CandidateWaiting from './components/shared/waiting/CandidateWaiting';
+
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
