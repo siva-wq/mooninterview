b6f7d915b81d47bd6eeb4bf4304ef208109c35f8
siva-wq <vullangulavenkatasivagangadhar@gmail.com>
2026-05-24 20:41:48 +0530

    Update SendMail.js

--- frontend/src/components/interview/SendMail.js
+++ frontend/src/components/interview/SendMail.js
@@ -5,7 +5,7 @@ const SendMail = async (candidate) => {
   try {
 
     const response = await axios.post(
-      'http://localhost:5000/api/send',
+      'https://mooninterview.onrender.com/api/send',
       {
         name: candidate.name,
         email: candidate.email,
