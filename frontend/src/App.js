cf063bc0d74cb9a4e6b00cbb6d0df790550117a7
siva-wq <vullangulavenkatasivagangadhar@gmail.com>
2026-06-25 07:48:35 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,11 @@
 import { Toaster } from "react-hot-toast";
 import './App.css';
 import AppRoutes from './routes/AppRoutes';
-import Home from './components/home/Home';
-
 
 
 function App() {
 
 
-  
   return (
     <>
       <Toaster
@@ -20,11 +17,5 @@ function App() {
   );
 }
 
-  {/*
-
-function App() {
-  return <Home />;
-}
-*/}
 
 export default App;
