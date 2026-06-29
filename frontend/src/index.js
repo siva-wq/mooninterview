891e9e5facfb623cd9722f3588e86ff007b0ef7c
siva-wq <vullangulavenkatasivagangadhar@gmail.com>
2026-06-29 08:01:00 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+import { HelmetProvider } from "react-helmet-async";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -6,9 +7,10 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
 root.render(
-  <App />
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
